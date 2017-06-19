c1eef22e1539be93d957765af9496874e4755859
Harry Denley <h.denley@yahoo.co.uk>
2017-06-19 22:15:33 +0100

    Removed code duplication

--- js/toggleMatchHighlight.js
+++ js/toggleMatchHighlight.js
@@ -1,5 +1,18 @@
 //On page load it checks/unchecks the checkbox
 (function() {
+    refreshHighlightOption();
+})();
+
+//Sets the local storage to remember their match highlight settings
+function toggleMatchHighlight() {
+    var objShowHighlight = document.getElementById("ext-etheraddresslookup-show_style");
+    var intShowHighlight = objShowHighlight.checked ? 1 : 0;
+    localStorage.setItem("ext-etheraddresslookup-show_style", intShowHighlight);
+
+    refreshHighlightOption();
+}
+
+function refreshHighlightOption() {
     var intShowHighlight = localStorage.getItem("ext-etheraddresslookup-show_style");
     document.getElementById("ext-etheraddresslookup-show_style").checked = (intShowHighlight == 1 ? true : false);
     //Notify the tab to do a class method
@@ -12,32 +25,4 @@
             console.log(response);
         });
     });
-})();
-
-//Sets the local storage to remember their match highlight settings
-function toggleMatchHighlight() {
-    var objShowHighlight = document.getElementById("ext-etheraddresslookup-show_style");
-    var intShowHighlight = objShowHighlight.checked ? 1 : 0;
-    localStorage.setItem("ext-etheraddresslookup-show_style", intShowHighlight);
-
-    //Notify the tab to do a class method
-    if(intShowHighlight == 1) {
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            //Construct & send message
-            chrome.tabs.sendMessage(tabs[0].id, {
-                "func":"addHighlightStyle"
-            }, function(response) {
-                console.log(response);
-            });
-        });
-    } else {
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            //Construct & send message
-            chrome.tabs.sendMessage(tabs[0].id, {
-                "func":"removeHighlightStyle"
-            }, function(response) {
-                console.log(response);
-            });
-        });
-    }
 }
