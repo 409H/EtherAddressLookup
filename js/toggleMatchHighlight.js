3fa2facdba9e6b516437d1b2d0eae148d43d3005
Harry Denley <h.denley@yahoo.co.uk>
2017-06-19 22:06:55 +0100

    Cleaned code up a little to remove duplication

--- js/toggleMatchHighlight.js
+++ js/toggleMatchHighlight.js
@@ -1,30 +1,18 @@
 //On page load it checks/unchecks the checkbox
-document.addEventListener("DOMContentLoaded", function() {
+(function() {
     var intShowHighlight = localStorage.getItem("ext-etheraddresslookup-show_style");
-    if(intShowHighlight == 1) {
-        document.getElementById("ext-etheraddresslookup-show_style").checked = true;
-        //Notify the tab to do a class method
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            //Construct & send message
-            chrome.tabs.sendMessage(tabs[0].id, {
-                "func":"addHighlightStyle"
-            }, function(response) {
-                console.log(response);
-            });
+    document.getElementById("ext-etheraddresslookup-show_style").checked = (intShowHighlight == 1 ? true : false);
+    //Notify the tab to do a class method
+    var strMethod = (intShowHighlight == 1 ? "addHighlightStyle" : "removeHighlightStyle");
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        //Construct & send message
+        chrome.tabs.sendMessage(tabs[0].id, {
+            "func":strMethod
+        }, function(response) {
+            console.log(response);
         });
-    } else {
-        document.getElementById("ext-etheraddresslookup-show_style").checked = false;
-        //Notify the tab to do a class method
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            //Construct & send message
-            chrome.tabs.sendMessage(tabs[0].id, {
-                "func":"removeHighlightStyle"
-            }, function(response) {
-                console.log(response);
-            });
-        });
-    }
-});
+    });
+})();
 
 //Sets the local storage to remember their match highlight settings
 function toggleMatchHighlight() {
