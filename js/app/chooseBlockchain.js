e6afea148cd7053de53c9bb1ba33a70f3faab7f1
Harry Denley <h.denley@yahoo.co.uk>
2017-06-28 22:19:11 +0100

    Added JS to handle changing the blockchain explorer

--- js/app/chooseBlockchain.js
+++ js/app/chooseBlockchain.js
@@ -1,22 +1,21 @@
-//On page load it checks/unchecks the checkbox
+//On page load it selects the default blockchain explorer
 (function() {
-    refreshHighlightOption();
+    refreshBlockchainExplorer();
 })();
 
-//Sets the local storage to remember their match highlight settings
-function toggleMatchHighlight() {
-    var objShowHighlight = document.getElementById("ext-etheraddresslookup-show_style");
-    var intShowHighlight = objShowHighlight.checked ? 1 : 0;
-    localStorage.setItem("ext-etheraddresslookup-show_style", intShowHighlight);
-
-    refreshHighlightOption();
+//Sets the local storage to remember their match favourite blockchain explorer
+function toggleBlockchainExplorer() {
+    var objBlockchainExplorer = document.getElementById("ext-etheraddresslookup-choose_blockchain");
+    localStorage.setItem("ext-etheraddresslookup-blockchain_explorer", objBlockchainExplorer.options[objBlockchainExplorer.selectedIndex].value);
+    refreshBlockchainExplorer();
 }
 
-function refreshHighlightOption() {
-    var intShowHighlight = localStorage.getItem("ext-etheraddresslookup-show_style");
-    document.getElementById("ext-etheraddresslookup-show_style").checked = (intShowHighlight == 1 ? true : false);
+function refreshBlockchainExplorer() {
+    var strBlockchainExplorer = localStorage.getItem("ext-etheraddresslookup-blockchain_explorer");
+    document.getElementById("ext-etheraddresslookup-choose_blockchain").value = strBlockchainExplorer;
+
     //Notify the tab to do a class method
-    var strMethod = (intShowHighlight == 1 ? "addHighlightStyle" : "removeHighlightStyle");
+    var strMethod = "changeBlockchainExplorer";
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, {
             "func":strMethod
