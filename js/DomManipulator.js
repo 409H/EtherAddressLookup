2d758d3cdfaf620329d22e235b8f9592904024fa
Samuel Hawksby-Robinson <samuel@samyoul.com>
2017-10-27 21:18:52 +0000

    Added ENS explorer compatibility check + bug fix
    Quite a lot of refactor work was done on this commit, and I should have just commited each part separately. But I wanted to fix the bug I found because it was messing up my testing.
    
    Bug Fix : basically stopped regex being applied to anything that wasn't a textNode, otherwise we would have attribute overwrites and other kinds of mess. In order for this to work I've created a tempory "slot" element that holds new elements, after regex is applied they are moved into the parent node.
    
    Added docblocks

--- js/DomManipulator.js
+++ js/DomManipulator.js
@@ -5,9 +5,7 @@ class EtherAddressLookup {
     constructor()
     {
         console.log("Init EAL");
-
         this.setDefaultExtensionSettings();
-        this.setSearchAndReplaceSettings();
         this.init();
     }
 
@@ -20,36 +18,17 @@ class EtherAddressLookup {
         this.intSettingsTotalCount = 2;
     }
 
-    setSearchAndReplaceSettings()
-    {
-        // Check user is on their favourite block explorer
-        // If we are on our favourite blockchain explorer, don't target blank.
-        this.onBlockchainExplorer = this.onBlockchainExplorer();
-        this.target = (this.onBlockchainExplorer ? '_self' : '_blank');
-
-        // Set RegExs
-        this.ethereumAddressRegex = new RegExp(/(^|\s|:|-)((?:0x)[0-9a-fA-F]{40})(?:\s|$)/gi, "gi");
-        this.ENSAddressRegex = new RegExp(/(\s|^|\.|\"|\'|\>)([a-z0-9][a-z0-9-\.]+[a-z0-9](?:.eth))(\s|$|\.|\"|\'|\<)/gim, "gim");
-
-        // Set string substitutions
-        this.ethereumAddressReplace = '$1<a title="See this address on the blockchain explorer" ' +
-            'href="' + this.strBlockchainExplorer + '/$2" ' +
-            'class="ext-etheraddresslookup-link" ' +
-            'target="'+ this.target +'">$2</a>';
-        this.ENSAddressReplace = '$1<a title="See this address on the blockchain explorer" ' +
-            'href="' + this.strBlockchainExplorer + '/$2" ' +
-            'class="ext-etheraddresslookup-link" ' +
-            'target="'+ this.target +'">$2</a>$3';
-    }
-
-    //Gets extension settings and then converts addresses to links
+    /**
+     * @name init
+     * @desc Gets extension settings and applies DOM manipulation
+     */
     init()
     {
         let objBrowser = chrome ? chrome : browser;
         //Get the highlight option for the user
         objBrowser.runtime.sendMessage({func: "highlight_option"}, function(objResponse) {
             if(objResponse && objResponse.hasOwnProperty("resp")) {
-                this.blHighlight = (objResponse.resp == 1 ? true : false);
+                this.blHighlight = (objResponse.resp == 1);
             }
             ++this.intSettingsCount;
         }.bind(this));
@@ -62,16 +41,99 @@ class EtherAddressLookup {
 
         //Update the DOM once all settings have been received...
         setTimeout(function() {
-            if(true || this.intSettingsCount === this.intSettingsTotalCount) {
-                if(this.blBlacklistDomains) {
-                    this.blacklistedDomainCheck();
-                }
-                this.convertAddressToLink();
-            }
-        }.bind(this), 10)
+            // Needs to happen after user settings have been collected
+            // and in the context of init();
+            this.setSearchAndReplaceSettings();
+            this.setWarningSettings();
+            this.manipulateDOM();
+        }.bind(this), 10);
     }
 
-    //Finds Ethereum addresses and converts to a link to a block explorer
+    /**
+     * @name Set Search And Replace Settings
+     * @desc
+     */
+    setSearchAndReplaceSettings()
+    {
+        // Check user is on their favourite block explorer, on fail target blank.
+        this.target = (this.isBlockchainExplorerSite() ? '_self' : '_blank');
+
+        // Register RegEx Patterns
+        this.regExPatterns = [
+            // Ethereum Address Regex
+            /(^|\s|:|-)((?:0x)[0-9a-fA-F]{40})(\s|$)/gi,
+
+            // ENS Address Regex
+            /([a-z0-9][a-z0-9-\.]+[a-z0-9](?:\.eth))(\s|$)/gi
+        ];
+
+        // Register RegEx Matching Patterns
+        this.matchPatterns = [
+            // Ethereum Match Pattern
+            /((?:0x)[0-9a-fA-F]{40})/gi,
+
+            // ENS Match Pattern
+            this.regExPatterns[1]
+        ];
+
+        // Register Replace Patterns
+        this.replacePatterns = [
+            // Ethereum Address Replace
+            '$1<a title="See this address on the blockchain explorer" ' +
+            'href="' + this.strBlockchainExplorer + '/$2" ' +
+            'class="ext-etheraddresslookup-link" ' +
+            'target="'+ this.target +'">$2</a>$3',
+
+            // ENS Address Replace
+            '<a title="See this address on the blockchain explorer" ' +
+            'href="' + this.strBlockchainExplorer + '/$1" ' +
+            'class="ext-etheraddresslookup-link" ' +
+            'target="'+ this.target +'">$1</a>$2'
+        ];
+    }
+
+    /**
+     * @name Set Warning Settings
+     * @desc
+     */
+    setWarningSettings()
+    {
+        // The block explorers that can handle ENS addresses
+        this.ENSCompatiableExplorers = [
+            "https://etherscan.io/address",
+            "https://etherchain.org/account"
+        ];
+
+        // Does the user's favorite explorer support an ENS address
+        for(var i=0; i<this.ENSCompatiableExplorers.length; i++){
+            if(this.strBlockchainExplorer == this.ENSCompatiableExplorers[i]){
+                this.ENSCompatiable = true;
+                break;
+            }
+            this.ENSCompatiable = false;
+        }
+
+        // On failure give the user a warning.
+        if(!this.ENSCompatiable){
+            this.replacePatterns[1] = '<a title="Notification! We have spotted an ENS address, your current block explorer can\'t parse this address. Please choose a compatible block explorer." ' +
+                'class="ext-etheraddresslookup-link ext-etheraddresslookup-warning" href="#">$1</a>';
+        }
+    }
+
+    manipulateDOM()
+    {
+        if(true || this.intSettingsCount === this.intSettingsTotalCount) {
+            if(this.blBlacklistDomains) {
+                this.blacklistedDomainCheck();
+            }
+            this.convertAddressToLink();
+        }
+    }
+
+    /**
+     * @name Convert Address To Link
+     * @desc Finds Ethereum addresses and converts to a link to a block explorer
+     */
     convertAddressToLink()
     {
         var arrWhitelistedTags = ["code", "span", "p", "td", "li", "em", "i", "b", "strong", "small"];
@@ -81,45 +143,101 @@ class EtherAddressLookup {
             var objNodes = document.getElementsByTagName(arrWhitelistedTags[i]);
             //Loop through the whitelisted content
             for(var x=0; x<objNodes.length; x++) {
-
-                this.convertEthereumAddress(objNodes, x);
-                this.convertENSAddress(objNodes, x);
-
+                this.convertAddresses(objNodes[x]);
             }
         }
+
+        this.tidyUpSlots();
 
         if(this.blHighlight) {
             this.addHighlightStyle();
         }
     }
 
-    convertEthereumAddress(objNodes, index)
+    /**
+     * @name Convert Addresses
+     * @desc Takes a Node and checks if any of its children are textNodes. On success replace textNode with slot node
+     * @desc slot node contains regex replaced content; see generateReplacementContent()
+     * @param {Node} objNode
+     */
+    convertAddresses(objNode)
     {
-        this.convertAddress(
-            objNodes, index, /((?:0x)[0-9a-fA-F]{40})/gi, this.ethereumAddressRegex, this.ethereumAddressReplace
-        );
-    }
+        // Some nodes have non-textNode children
+        // we need to ensure regex is applied only to text otherwise we will mess the html up
+        for(var i=0; i < objNode.childNodes.length; i++){
+            // Only check textNodes to prevent applying RegEx against element attributes
+            if(objNode.childNodes[i].nodeType == 3){ // nodeType 3 = a text node
 
-    convertENSAddress(objNodes, index)
-    {
-        this.convertAddress(
-            objNodes, index, this.ENSAddressRegex, this.ENSAddressRegex, this.ENSAddressReplace
-        );
-    }
+                var child = objNode.childNodes[i];
+                var childContent = child.textContent;
 
-    convertAddress(objNodes, index, checkRegex, replaceRegex, replacePattern)
-    {
-        var strContent = objNodes[index].innerHTML;
-        if( checkRegex.exec(strContent) !== null) {
-            objNodes[index].innerHTML = strContent.replace(replaceRegex, replacePattern);
+                // Only start replacing stuff if the we get a RegEx match.
+                if(this.isPatternMatched(childContent)) {
+                    // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/slot
+                    var replacement = document.createElement('slot');
+                    replacement.setAttribute('class', 'ext-etheraddresslookup-temporary');
+                    replacement.innerHTML = this.generateReplacementContent(childContent);
+                    objNode.replaceChild(replacement, child);
+                }
+            }
         }
     }
 
-    onBlockchainExplorer()
+    /**
+     * @name Generate Replacement Content
+     * @desc Takes string and replaces any regex pattern matches with the associated replace patterns
+     * @param {string} content
+     * @returns {string}
+     */
+    generateReplacementContent(content)
+    {
+        for(var i=0; i < this.regExPatterns.length; i++){
+            content = content.replace(this.regExPatterns[i], this.replacePatterns[i]);
+        }
+        return content;
+    }
+
+    /**
+     * @name Is Pattern Matched
+     * @desc Checks content matches any of the object's matchPatterns
+     * @param {string} content
+     * @returns {boolean}
+     */
+    isPatternMatched(content)
+    {
+        for(var i=0; i < this.matchPatterns.length; i++){
+            if(this.matchPatterns[i].exec(content) !== null){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * @name Is Blockchain Explorer Site
+     * @desc Check if the current website is the user's selected block explorer
+     * @returns {boolean}
+     */
+    isBlockchainExplorerSite()
     {
         var objBlockchainExplorer = document.createElement("a");
         objBlockchainExplorer.href = this.strBlockchainExplorer;
-        return objBlockchainExplorer.hostname === window.location.hostname;
+        return (objBlockchainExplorer.hostname === window.location.hostname);
+    }
+
+    /**
+     * @name Tidy Slots
+     * @desc Searches document for slots adds the slot's child nodes to its parent then removes the slot
+     */
+    tidyUpSlots()
+    {
+        var slots = document.querySelectorAll("slot.ext-etheraddresslookup-temporary");
+        for(var i=0; i < slots.length; i++){
+            while(slots[i].childNodes.length > 0){
+                slots[i].parentNode.appendChild(slots[i].firstChild);
+            }
+            slots[i].parentNode.removeChild(slots[i]);
+        }
     }
 
     //Removes the highlight style from Ethereum addresses
