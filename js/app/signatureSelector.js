99c8c5de56927bc8c4bc4853193b5901f7de0286
409H <harry.denley@outlook.com>
2018-05-16 16:19:54 +0100

    Fixed logic to pre-tick/untick the checkbox based on the users setting

--- js/app/signatureSelector.js
+++ js/app/signatureSelector.js
@@ -14,7 +14,8 @@ class SignatureSelector
         document.getElementById("ext-etheraddresslookup-signature_success").classList.add("hide-me");
 
         objBrowser.runtime.sendMessage({func: "signature_inject"}, function(objResponse) {
-            document.getElementById("ext-etheraddresslookup-signature_modify_checkbox").checked = objResponse.resp;
+            console.log(objResponse);
+            document.getElementById("ext-etheraddresslookup-signature_modify_checkbox").checked = !!parseInt(objResponse.resp);
         });
     }
 
