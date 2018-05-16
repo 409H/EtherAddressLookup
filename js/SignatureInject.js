5a4682af3421ed3c513e1a8fef32fa5d5acdffe4
409H <harry.denley@outlook.com>
2018-05-16 18:32:44 +0100

    Added logic to only append to body when the window is loaded

--- js/SignatureInject.js
+++ js/SignatureInject.js
@@ -3,12 +3,14 @@
     //See if they have the signature injection enabled
     objBrowser.runtime.sendMessage({func: "signature_inject"}, function(objResponse) {
         if(objResponse.resp === "1") {
-            var strSignature = "<!--- EAL IS INSTALLED -->";
-            if (document.getElementById("ext-etheraddresslookup-signature") === null) {
-                var objSignatureDiv = document.createElement('div');
-                objSignatureDiv.id = "ext-etheraddresslookup-signature";
-                objSignatureDiv.innerHTML = strSignature;
-                document.body.appendChild(objSignatureDiv);
+            window.onload = function() {
+                var strSignature = "<!--- EAL IS INSTALLED -->";
+                if (document.getElementById("ext-etheraddresslookup-signature") === null) {
+                    var objSignatureDiv = document.createElement('div');
+                    objSignatureDiv.id = "ext-etheraddresslookup-signature";
+                    objSignatureDiv.innerHTML = strSignature;
+                    document.body.appendChild(objSignatureDiv);
+                }
             }
         }
     });
