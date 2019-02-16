acf87f891b7ce44d7a3164964d562a46642b547c
409H <harry.denley@outlook.com>
2019-02-16 21:00:24 +0000

    Added logic to perform ENS validation and lookups

---
+++ js/ens.js
@@ -0,0 +1,133 @@
+const ENS_KEY = 'ens';
+const FORM_ENS_NAME_SELECTOR = '#ext-etheraddresslookup-ens_name';
+const ENS_REGISTRY = {
+    "mainnet": {
+        "addr": '0x314159265dd8dbb310642f98f50c066173c1259b',
+        "abi": '[{"constant":true,"inputs":[{"name":"node","type":"bytes32"}],"name":"resolver","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"node","type":"bytes32"}],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"node","type":"bytes32"},{"name":"label","type":"bytes32"},{"name":"owner","type":"address"}],"name":"setSubnodeOwner","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"node","type":"bytes32"},{"name":"ttl","type":"uint64"}],"name":"setTTL","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"node","type":"bytes32"}],"name":"ttl","outputs":[{"name":"","type":"uint64"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"node","type":"bytes32"},{"name":"resolver","type":"address"}],"name":"setResolver","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"node","type":"bytes32"},{"name":"owner","type":"address"}],"name":"setOwner","outputs":[],"payable":false,"type":"function"},{"anonymous":false,"inputs":[{"indexed":true,"name":"node","type":"bytes32"},{"indexed":false,"name":"owner","type":"address"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"node","type":"bytes32"},{"indexed":true,"name":"label","type":"bytes32"},{"indexed":false,"name":"owner","type":"address"}],"name":"NewOwner","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"node","type":"bytes32"},{"indexed":false,"name":"resolver","type":"address"}],"name":"NewResolver","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"node","type":"bytes32"},{"indexed":false,"name":"ttl","type":"uint64"}],"name":"NewTTL","type":"event"}]'
+    },
+    "ropsten": {
+        "addr": '0x112234455c3a32fd11230c42e7bccd4a84e02010',
+        "abi": ''
+    },
+    "rinkeby": {
+        "addr": '0xe7410170f87102df0055eb195163a03b7f2bff4a',
+        "abi": ''
+    }
+};
+const ENS_RESOLVER = {
+    "0x5ffc014343cd971b7eb70732021e26c35b744cc4": {
+        "abi": '[{"constant":true,"inputs":[{"name":"interfaceID","type":"bytes4"}],"name":"supportsInterface","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"node","type":"bytes32"},{"name":"key","type":"string"},{"name":"value","type":"string"}],"name":"setText","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"node","type":"bytes32"},{"name":"contentTypes","type":"uint256"}],"name":"ABI","outputs":[{"name":"contentType","type":"uint256"},{"name":"data","type":"bytes"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"node","type":"bytes32"},{"name":"x","type":"bytes32"},{"name":"y","type":"bytes32"}],"name":"setPubkey","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"node","type":"bytes32"}],"name":"content","outputs":[{"name":"ret","type":"bytes32"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"node","type":"bytes32"}],"name":"addr","outputs":[{"name":"ret","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"node","type":"bytes32"},{"name":"key","type":"string"}],"name":"text","outputs":[{"name":"ret","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"node","type":"bytes32"},{"name":"contentType","type":"uint256"},{"name":"data","type":"bytes"}],"name":"setABI","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"node","type":"bytes32"}],"name":"name","outputs":[{"name":"ret","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"node","type":"bytes32"},{"name":"name","type":"string"}],"name":"setName","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"node","type":"bytes32"},{"name":"hash","type":"bytes32"}],"name":"setContent","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"node","type":"bytes32"}],"name":"pubkey","outputs":[{"name":"x","type":"bytes32"},{"name":"y","type":"bytes32"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"node","type":"bytes32"},{"name":"addr","type":"address"}],"name":"setAddr","outputs":[],"payable":false,"type":"function"},{"inputs":[{"name":"ensAddr","type":"address"}],"payable":false,"type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"name":"node","type":"bytes32"},{"indexed":false,"name":"a","type":"address"}],"name":"AddrChanged","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"node","type":"bytes32"},{"indexed":false,"name":"hash","type":"bytes32"}],"name":"ContentChanged","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"node","type":"bytes32"},{"indexed":false,"name":"name","type":"string"}],"name":"NameChanged","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"node","type":"bytes32"},{"indexed":true,"name":"contentType","type":"uint256"}],"name":"ABIChanged","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"node","type":"bytes32"},{"indexed":false,"name":"x","type":"bytes32"},{"indexed":false,"name":"y","type":"bytes32"}],"name":"PubkeyChanged","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"node","type":"bytes32"},{"indexed":true,"name":"indexedKey","type":"string"},{"indexed":false,"name":"key","type":"string"}],"name":"TextChanged","type":"event"}]'
+    }
+}
+
+class Ens {
+
+    web3;
+
+    constructor()
+    {
+        chrome.runtime.sendMessage({ func: "rpc_provider" }, (objResponse) => {
+            this.web3 = new Web3(new Web3.providers.HttpProvider(objResponse.resp)); 
+            this.setupFormSubmitHandler();
+        });
+    }
+
+    setupFormSubmitHandler()
+    {
+        document.getElementById('ext-etheraddresslookup-ens_lookup_form').addEventListener('submit', async (event) => {
+            event.preventDefault();
+
+            const ensname = document.querySelector(FORM_ENS_NAME_SELECTOR).value;
+            await this.resolve(ensname);
+        });
+    }
+
+    async resolve(strEnsName)
+    {
+        let strOutput;
+        strEnsName = this.normalizeAndValidate(strEnsName);
+        let objNameHashes = this.getNameHash(strEnsName);
+        this.getEnsResolver(objNameHashes.namehash);
+    }
+
+    normalizeAndValidate(strEnsName)
+    {
+        strEnsName = strEnsName.toLowerCase();
+        document.querySelector("#ext-etheraddresslookup-ens_output").innerHTML = ``;
+
+        if(/^([a-z0-9]+\.?)+\.eth$/gm.exec(strEnsName) === null) {
+            document.querySelector("#ext-etheraddresslookup-ens_output").innerHTML = `
+                <span class="error">Invalid ENS name - invalid characters used.</span>
+            `;
+            return;
+        }
+
+        return strEnsName;
+    }
+
+    getNameHash(strEnsName)
+    {
+        // @todo - create the namehash correctly
+        let objEnsName = {
+            "labels": {},
+            "namehash": "0x4304299c9f838df31435eb3e8d79705f28960b6ea59329d33828cca28b7fcb76"
+        };
+
+        // Get the hash of each label
+        var arrLabels = strEnsName.split(".");
+        arrLabels.map(l => {
+            objEnsName.labels[l] = this.web3.sha3(l);
+        });
+
+        //objEnsName.namehash = this.web3.sha3(strEnsName);
+
+        return objEnsName;
+    }
+
+    getEnsResolver(strNameHash)
+    {
+        // @todo - get the network ID for the web3 provider
+        let objContract = this.web3.eth.contract(JSON.parse(ENS_REGISTRY.mainnet.abi));
+        let objContractInst = objContract.at(ENS_REGISTRY.mainnet.addr);
+
+        objContractInst.resolver.call(strNameHash, (err, res) => {
+            if(err) {
+                document.querySelector("#ext-etheraddresslookup-ens_output").innerHTML = `
+                    <span class="error">Error - ${err}</span>
+                `;
+                return;
+            }
+    
+            if(res === "0x"+"0".repeat(40)) { // No resolver set (assuming name doesn't exist)
+                document.querySelector("#ext-etheraddresslookup-ens_output").innerHTML = `
+                    <span class="error">Error - No resolver is set!</span>
+                `;
+                return;
+            }
+
+            let objResolverContract = this.web3.eth.contract(JSON.parse(ENS_RESOLVER[res].abi));
+            let objResolverContractInst = objResolverContract.at(res);
+
+            objResolverContractInst.addr.call(strNameHash, (err, res) => {
+                if(err) {
+                    document.querySelector("#ext-etheraddresslookup-ens_output").innerHTML = `
+                        <span class="error">Error - ${err}</span>
+                    `;
+                    return;
+                }
+
+                let strBlockie = blockies.create({
+                    // toLowerCase is used because standard blockies are based on none-checksum Ethereum addresses
+                    seed: res.toLowerCase(),
+                    size: 8,
+                    scale: 16
+                }).toDataURL();
+                document.querySelector("#ext-etheraddresslookup-ens_output").innerHTML += `
+                    <strong>Address:</strong> <br />
+                    <span><img id="blockie" src="${strBlockie}" /> ${res}</span>
+                `;
+            });
+
+        });
+    }
+}
